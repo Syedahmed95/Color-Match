389657a0daf333e3f8c9187141aebf376e95bb17
syedahmed95 <syedahmed978@msn.com>
2019-09-21 02:36:27 +0500

    Updates

---
+++ precache-manifest.20ac9eac7eb1070ab0e9e82f0a1648f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1900dc506437e60cdd400b62082d657",
+    "url": "/Color-Match/./index.html"
+  },
+  {
+    "revision": "53de2d52f5c19f7675d8",
+    "url": "/Color-Match/./static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "892ff236825ed12aee1a",
+    "url": "/Color-Match/./static/css/main.a11ac1e0.chunk.css"
+  },
+  {
+    "revision": "53de2d52f5c19f7675d8",
+    "url": "/Color-Match/./static/js/2.28d6b681.chunk.js"
+  },
+  {
+    "revision": "892ff236825ed12aee1a",
+    "url": "/Color-Match/./static/js/main.e005baf1.chunk.js"
+  },
+  {
+    "revision": "ac56d7c615667718e06c",
+    "url": "/Color-Match/./static/js/runtime~main.2ef3da30.js"
+  }
+]);
